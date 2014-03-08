86bc5575af9dc41c3b6b84caffa2198f91dfc86c
averoom <averoooom@outlook.jp>
2014-03-08 19:58:46 +0900

    POST → GET

--- html/xmloutput.php
+++ html/xmloutput.php
@@ -7,37 +7,37 @@ $xml = @simplexml_load_file($copyname);
 if ($xml) {
   // 正常に読み込めた場合の処理
   //GlobalKeyColor
-  $xml->GlobalKeyColor = str_replace('#', '#FF', $_POST['krile']);
+  $xml->GlobalKeyColor = str_replace('#', '#FF', $_GET['krile']);
   //TitleBar
-  $xml->TitleBarColor->BackgroundColor = str_replace('#', '#FF', $_POST['titlebar']);
+  $xml->TitleBarColor->BackgroundColor = str_replace('#', '#FF', $_GET['titlebar']);
 
-  $xml->BaseColor->BackgroundColor = str_replace('#', '#FF', $_POST['krile_content']);
+  $xml->BaseColor->BackgroundColor = str_replace('#', '#FF', $_GET['krile_content']);
   //Tab
-  $xml->TabColor->DefaultColor = str_replace('#', '#33', $_POST['tab']);
-  $xml->TabColor->SelectedColor = str_replace('#', '#88', $_POST['tab']);
-  $xml->TabColor->FocusedColor = str_replace('#', '#FF', $_POST['tab']);
+  $xml->TabColor->DefaultColor = str_replace('#', '#33', $_GET['tab']);
+  $xml->TabColor->SelectedColor = str_replace('#', '#88', $_GET['tab']);
+  $xml->TabColor->FocusedColor = str_replace('#', '#FF', $_GET['tab']);
   //Screen Name
-  $xml->TweetDefaultColor->ForegroundKeyColor = str_replace('#', '#FF', $_POST['tldefault']);
-  $xml->TweetMyselfColor->ForegroundKeyColor = str_replace('#', '#FF', $_POST['tlmyself']);
-  $xml->TweetMentionColor->ForegroundKeyColor = str_replace('#', '#FF', $_POST['tlmention']);
-  $xml->TweetRetweetColor->ForegroundKeyColor = str_replace('#', '#FF', $_POST['tlretweet']);
-  $xml->TweetDirectColor->ForegroundKeyColor = str_replace('#', '#FF', $_POST['tldirect']);
+  $xml->TweetDefaultColor->ForegroundKeyColor = str_replace('#', '#FF', $_GET['tldefault']);
+  $xml->TweetMyselfColor->ForegroundKeyColor = str_replace('#', '#FF', $_GET['tlmyself']);
+  $xml->TweetMentionColor->ForegroundKeyColor = str_replace('#', '#FF', $_GET['tlmention']);
+  $xml->TweetRetweetColor->ForegroundKeyColor = str_replace('#', '#FF', $_GET['tlretweet']);
+  $xml->TweetDirectColor->ForegroundKeyColor = str_replace('#', '#FF', $_GET['tldirect']);
   //TimeLine Default Color
-  $xml->TweetMyselfColor->Default->BackgroundColor = str_replace('#', '#16', $_POST['tlmyself']);
-  $xml->TweetMentionColor->Default->BackgroundColor = str_replace('#', '#16', $_POST['tlmention']);
-  $xml->TweetRetweetColor->Default->BackgroundColor = str_replace('#', '#16', $_POST['tlretweet']);
-  $xml->TweetDirectColor->Default->BackgroundColor = str_replace('#', '#16', $_POST['tldirect']);
+  $xml->TweetMyselfColor->Default->BackgroundColor = str_replace('#', '#16', $_GET['tlmyself']);
+  $xml->TweetMentionColor->Default->BackgroundColor = str_replace('#', '#16', $_GET['tlmention']);
+  $xml->TweetRetweetColor->Default->BackgroundColor = str_replace('#', '#16', $_GET['tlretweet']);
+  $xml->TweetDirectColor->Default->BackgroundColor = str_replace('#', '#16', $_GET['tldirect']);
   //TimeLine Default TextColor
-  $xml->TweetDefaultColor->Default->ForegroundColor = str_replace('#', '#FF', $_POST['tweet']);
-  $xml->TweetMyselfColor->Default->ForegroundColor = str_replace('#', '#FF', $_POST['tweet']);
-  $xml->TweetMentionColor->Default->ForegroundColor = str_replace('#', '#FF', $_POST['tweet']);
-  $xml->TweetRetweetColor->Default->ForegroundColor = str_replace('#', '#FF', $_POST['tweet']);
-  $xml->TweetDirectColor->Default->ForegroundColor = str_replace('#', '#FF', $_POST['tweet']);
+  $xml->TweetDefaultColor->Default->ForegroundColor = str_replace('#', '#FF', $_GET['tweet']);
+  $xml->TweetMyselfColor->Default->ForegroundColor = str_replace('#', '#FF', $_GET['tweet']);
+  $xml->TweetMentionColor->Default->ForegroundColor = str_replace('#', '#FF', $_GET['tweet']);
+  $xml->TweetRetweetColor->Default->ForegroundColor = str_replace('#', '#FF', $_GET['tweet']);
+  $xml->TweetDirectColor->Default->ForegroundColor = str_replace('#', '#FF', $_GET['tweet']);
   //TimeLine Highlight
-  $xml->TweetMyselfColor->Highlight->BackgroundColor = str_replace('#', '#20', $_POST['tlmyself']);
-  $xml->TweetMentionColor->Highlight->BackgroundColor = str_replace('#', '#20', $_POST['tlmention']);
-  $xml->TweetRetweetColor->Highlight->BackgroundColor = str_replace('#', '#20', $_POST['tlretweet']);
-  $xml->TweetDirectColor->Highlight->BackgroundColor = str_replace('#', '#20', $_POST['tldirect']);
+  $xml->TweetMyselfColor->Highlight->BackgroundColor = str_replace('#', '#20', $_GET['tlmyself']);
+  $xml->TweetMentionColor->Highlight->BackgroundColor = str_replace('#', '#20', $_GET['tlmention']);
+  $xml->TweetRetweetColor->Highlight->BackgroundColor = str_replace('#', '#20', $_GET['tlretweet']);
+  $xml->TweetDirectColor->Highlight->BackgroundColor = str_replace('#', '#20', $_GET['tldirect']);
   //上書き保存
   $xml->asXml($copyname);
 
